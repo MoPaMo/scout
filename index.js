54d8d90933db4efcbe6f8bd6cca95fbe1ef79159
MoPaMo <mopamo@users.noreply.github.com>
2024-10-27 10:44:55 +0100

    Refactor package.json and index.js to add mustache dependency and template loading

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const path = require("path");
+const Mustache = require("mustache");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
+
+//load templates from views
+const templates = {};
+
+// Preload templates
+function preloadTemplates(templateDir) {
+  const files = fs.readdirSync(templateDir);
+
+  files.forEach((file) => {
+    const filePath = path.join(templateDir, file);
+    const templateName = path.basename(file, ".mustache");
+
+    templates[templateName] = fs.readFileSync(filePath, "utf8");
+  });
+}
+
+// Initialize templates on startup
+preloadTemplates("./views");
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,9 +36,12 @@ app.get("/search", (req, res) => {
   //send index.html file
   res.sendFile(path.join(__dirname, "views", "search.html"));
 });
-app.get("/lecture/abc", (req, res) => {
+
+app.get("/lecture/:number/:part?/:name?", (req, res) => {
+  const { number, part = 1, name } = req.params;
+  console.log({ number, part, name });
   //send index.html file
-  res.sendFile(path.join(__dirname, "views", "lecture.html"));
+  res.send(Mustache.render(templates.lecture, { number, part, name }));
 });
 
 app.get("/about", (req, res) => {
